bf501e228191ec4ecd346e024af047af35b75d2d
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-11-21 20:58:38 -0500

    testing to make sure styling is applied

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            Santa's Workshop
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+  import HomePage from './components/HomePage.vue';
+  
+  export default {
+    name: 'app',
+    components: {
+      HomePage,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: rgb(148, 43, 43);
+  }
+
+  nav li {
+    list-style: none;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    margin-top: 60px;
+  }
 </style>
