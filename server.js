a4825c13c89fdfffa757bbb4a5bb0974402a8ed2
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-11-22 22:10:39 -0500

    come on heroku GET IT TOGETHER

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const serveStatic = require('serve-static');
 
 app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) );
 
 const port = process.env.PORT || 8080;
 
