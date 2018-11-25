00beeebc5d69dcfdcab8acd67e63ecf9966dbf71
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-11-25 18:44:45 -0500

    trying to fix port issue

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
   res.sendFile(`${__dirname }/dist/index.html`);
 });
 
-const port = process.env.PORT || 8080
+const port = 5000
 app.listen(port)
 
 console.log('Server started on port ' + port) 
